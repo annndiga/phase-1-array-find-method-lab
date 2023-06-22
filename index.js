418c04b5eaecad38747541fcf89dbba62827affb
Ann Ndiga <annndiga@gmail.com>
2023-06-22 20:40:16 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+    function superbowlWin (collection){
+        for (const item of collection) {
+            if (item.result==="W") {
+                return item.year
+                {
+                    return undefined
+                }
+            }
+            }
+        }
+
+    
+        record.find(superbowlWin)
+   
+
+
+
